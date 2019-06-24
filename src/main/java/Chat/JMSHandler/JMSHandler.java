42059ee7256b56aab89d605bf417b22f1e68e092
F0urth <f0urth.kamil@gmail.com>
2019-06-25 00:33:18 +0200

    Add JMSHandler class definition

--- src/main/java/Chat/JMSHandler/JMSHandler.java
+++ src/main/java/Chat/JMSHandler/JMSHandler.java
@@ -1,9 +1,36 @@
 package Chat.JMSHandler;
 
+import org.apache.camel.Message;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.rx.ReactiveCamel;
+import rx.Observable;
+
+
 /**
  * @author F0urth
  */
 public
     class JMSHandler {
 
+    private DefaultCamelContext camelContext;
+    private ReactiveCamel reactiveCamel;
+
+
+    public Observable<Message> getObservableOf() {
+        return reactiveCamel
+            .toObservable("activemq:topic:JMSChat");
+    }
+
+    /**
+     * Factory method
+     * @return new Instance
+     */
+    public static JMSHandler newInstance() {
+        return new JMSHandler();
+    }
+
+    private JMSHandler() {
+        this.camelContext = new DefaultCamelContext();
+        this.reactiveCamel = new ReactiveCamel(this.camelContext);
+    }
 }
