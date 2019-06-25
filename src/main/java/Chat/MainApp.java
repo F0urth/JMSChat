a330f15eb0e1e93364ff0b509f974ad4e3ad243b
F0urth <f0urth.kamil@gmail.com>
2019-06-25 02:58:14 +0200

    Add exit call to primary stage

--- src/main/java/Chat/MainApp.java
+++ src/main/java/Chat/MainApp.java
@@ -1,10 +1,13 @@
 package Chat;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 /**
  * @author F0urth
@@ -15,7 +18,9 @@ public
         extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setOnCloseRequest(
+            event -> System.exit(0));
         Parent root = FXMLLoader.load(getClass().getResource("/Views/MainFrame.fxml"));
         primaryStage.setTitle("Chat");
         primaryStage.setScene(new Scene(root, 600, 600));
